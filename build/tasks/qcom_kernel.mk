b5e894a659fe013417ddcd84f6d217edbfda0270
Pavel Dubrova <pashadubrova@gmail.com>
2020-10-13 18:41:35 +0300

    build: Remove deprecated kernel build rule
    From now we use kernel build scripts only.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- build/tasks/qcom_kernel.mk
+++ build/tasks/qcom_kernel.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2019 Pavel Dubrova <pashadubrova@gmail.com>
+# Copyright (C) 2019-2020 Pavel Dubrova <pashadubrova@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,61 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# If the kernel_definitions.mk is not found, fallback to the
-# current kernel build instruction.
-ifneq ($(wildcard device/qcom/kernelscripts/kernel_definitions.mk),)
-TARGET_KERNEL_PATH := device/qcom/kernelscripts/kernel_definitions.mk
-else
-
 ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
-#----------------------------------------------------------------------
-# Host compiler configs
-#----------------------------------------------------------------------
-SOURCE_ROOT := $(shell pwd)
-TARGET_HOST_COMPILER_PREFIX_OVERRIDE := prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.17-4.8/bin/x86_64-linux-
-TARGET_HOST_CC_OVERRIDE := $(TARGET_HOST_COMPILER_PREFIX_OVERRIDE)gcc
-TARGET_HOST_CXX_OVERRIDE := $(TARGET_HOST_COMPILER_PREFIX_OVERRIDE)g++
-TARGET_HOST_AR_OVERRIDE := $(TARGET_HOST_COMPILER_PREFIX_OVERRIDE)ar
-TARGET_HOST_LD_OVERRIDE := $(TARGET_HOST_COMPILER_PREFIX_OVERRIDE)ld
-
-TARGET_KERNEL_MAKE_ENV += HOSTCC=$(SOURCE_ROOT)/$(TARGET_HOST_CC_OVERRIDE)
-TARGET_KERNEL_MAKE_ENV += HOSTAR=$(SOURCE_ROOT)/$(TARGET_HOST_AR_OVERRIDE)
-TARGET_KERNEL_MAKE_ENV += HOSTLD=$(SOURCE_ROOT)/$(TARGET_HOST_LD_OVERRIDE)
-TARGET_KERNEL_MAKE_ENV += HOSTCFLAGS="-I/usr/include -I/usr/include/x86_64-linux-gnu -L/usr/lib -L/usr/lib/x86_64-linux-gnu"
-TARGET_KERNEL_MAKE_ENV += HOSTLDFLAGS="-L/usr/lib -L/usr/lib/x86_64-linux-gnu"
-TARGET_KERNEL_MAKE_ENV += PERL=/usr/bin/perl
-
-#----------------------------------------------------------------------
-# Define defconfig
-#----------------------------------------------------------------------
-ifeq ($(KERNEL_DEFCONFIG),)
-   ifeq ($(TARGET_BUILD_VARIANT),user)
-     KERNEL_DEFCONFIG := $(PRODUCT_PLATFORM)_$(PRODUCT_DEVICE)-perf_defconfig
-   else
-     KERNEL_DEFCONFIG := $(PRODUCT_PLATFORM)_$(PRODUCT_DEVICE)_defconfig
-   endif
-endif
-
-#----------------------------------------------------------------------
-# Check if all necessary flags have been defined
-#----------------------------------------------------------------------
-ifeq ($(TARGET_KERNEL_SOURCE),)
-    $(error Kernel sorce path not defined, cannot build kernel)
-else
-    $(warning Kernel source tree path is: $(TARGET_KERNEL_SOURCE))
-endif
-
-ifeq ($(TARGET_KERNEL_VERSION),)
-    $(error Kernel version not defined, cannot build kernel)
-else
-    $(warning Kernel version is: $(TARGET_KERNEL_VERSION))
-endif
-
-ifeq ($(KERNEL_DEFCONFIG),)
-    $(error Kernel defconfig not defined, cannot build kernel)
-else
-    $(warning Kernel defconfig is: $(KERNEL_DEFCONFIG))
-endif
 
 #----------------------------------------------------------------------
 # Create Kernel symlink
@@ -74,13 +20,10 @@ endif
 $(shell rm -fr $(shell pwd)/kernel/msm-$(TARGET_KERNEL_VERSION))
 $(shell ln -s $(shell pwd)/$(TARGET_KERNEL_SOURCE) $(shell pwd)/kernel/msm-$(TARGET_KERNEL_VERSION))
 
-#----------------------------------------------------------------------
-# Compile Linux Kernel
-#----------------------------------------------------------------------
-include $(TARGET_KERNEL_SOURCE)/AndroidKernel.mk
-
-$(INSTALLED_KERNEL_TARGET): $(TARGET_PREBUILT_KERNEL) | $(ACP)
-	$(transform-prebuilt-to-target)
+ifneq ($(wildcard device/qcom/kernelscripts/kernel_definitions.mk),)
+    include $(device/qcom/kernelscripts/kernel_definitions.mk)
+else
+    $(error Kernel scripts not found, cannot build kernel)
+endif
 
 endif # TARGET_COMPILE_WITH_MSM_KERNEL
-endif # kernel_definitions.mk wildcard
