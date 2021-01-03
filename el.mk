5b7b06296a9d072056b986ebfc91f8b80ef7b909
Pavel Dubrova <pashadubrova@gmail.com>
2021-01-03 12:10:41 +0200

    packages: Build SnapdragonCamera/Gallery/Music apps
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- el.mk
+++ el.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2020 Pavel Dubrova <pashadubrova@gmail.com>
+# Copyright (C) 2020-2021 Pavel Dubrova <pashadubrova@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,4 +14,7 @@
 
 # Extra packages
 PRODUCT_PACKAGES += \
-    ExactCalculator
+    ExactCalculator \
+    SnapdragonCamera \
+    SnapdragonGallery \
+    SnapdragonMusic
